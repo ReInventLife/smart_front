149c91059e856714afe8fa54acd48b46933fde63
Zerubabel <zerucodes@gmail.com>
2024-04-16 15:26:52 +0300

    private auth updated

--- src/markup/components/Auth/PrivateAuthRoute.jsx
+++ src/markup/components/Auth/PrivateAuthRoute.jsx
@@ -3,7 +3,38 @@ import React, { useState, useEffect } from "react";
 // Import the Route and Navigate components
 import { Navigate } from "react-router";
 // Import the Util function we created to handle the reading from the local storage
-import getAuth from "../../../util/auth";
+// import getAuth from "../../../util/auth";
+
+const getAuth = async () => {
+  const employee = await JSON.parse(localStorage.getItem("employee"));
+  // console.log(employee.data.employee_token);
+  if (employee && employee.data.employee_token) {
+    // console.log(employee.data.employee_token);
+    const decodedToken = await decodeTokenPayload(employee.data.employee_token);
+    employee.employee_role = decodedToken.employee_role;
+    employee.employee_id = decodedToken.employee_id;
+    employee.employee_first_name = decodedToken.employee_first_name;
+    // console.log(employee);
+    return employee;
+  } else {
+    return {};
+  }
+};
+
+// Function to decode the payload from the token
+// The purpose of this code is to take a JWT token, extract its payload, decode it from Base64Url encoding, and then convert the decoded payload into a JavaScript object for further use and manipulation
+const decodeTokenPayload = (token) => {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map((c) => `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`)
+      .join("")
+  );
+  // console.log("Decodedddd token payload:", JSON.parse(jsonPayload));
+  return JSON.parse(jsonPayload);
+};
 
 const PrivateAuthRoute = ({ roles, children }) => {
   const [isChecked, setIsChecked] = useState(false);
